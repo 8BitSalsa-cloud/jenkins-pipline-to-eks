6110fd4c8d2fa32a0db216518608d080eb3157ca
Jens <salsa.programming@gmail.com>
2023-09-18 14:20:13 +0200

    correct eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,17 +1,17 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
+    cluster_name = "iwap-eks-cluster"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
 
-    vpc_id = module.myapp-vpc.vpc_id
-    subnet_ids = module.myapp-vpc.private_subnets
+    vpc_id = module.iwap-vpc.vpc_id
+    subnet_ids = module.iwap-vpc.private_subnets
 
     tags = {
         environment = "development"
-        application = "myapp"
+        application = "iwap-jens"
     }
 
     eks_managed_node_groups = {
